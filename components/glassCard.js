fe619c6601732ef55ff18741840014d53954d9e4
Ivan Joly <ivan.joly.dm@gmail.com>
2023-02-16 11:44:21 -0300

    fix: remove error forwardref on glasscard

--- components/glassCard.js
+++ components/glassCard.js
@@ -1,6 +1,7 @@
 import { forwardRef, Box } from '@chakra-ui/react';
 
-const GlassCard = forwardRef(({ children, ref, width, height, ...props }) => {
+const GlassCard = forwardRef((props, ref) => {
+    const { children, width, height } = props;
     return (
         <Box position={'relative'} borderRadius={'16px'} overflow={'hidden'} width={width} height={height} ref={ref} {...props}>
             <Box layerStyle="glass" position={'absolute'} top={'50%'} left={'50%'} transform={'translate(-50%, -50%)'} zIndex={0} width={'inherit'} height={'inherit'}></Box>
