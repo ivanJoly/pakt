5604bc658e465bbdd7f607601c7a978c6674c862
Ivan Joly <ivan.joly.dm@gmail.com>
2023-02-21 17:02:19 -0300

    add: on hover icons

--- components/glassCard.js
+++ components/glassCard.js
@@ -1,11 +1,39 @@
 import { forwardRef, Box } from '@chakra-ui/react';
 
 const GlassCard = forwardRef((props, ref) => {
-    const { children, width, height } = props;
+    const { children, width, height, isOnHoverActive = false } = props;
+
     return (
-        <Box position={'relative'} borderRadius={'16px'} overflow={'hidden'} width={width} height={height} ref={ref} {...props}>
-            <Box layerStyle="glass" position={'absolute'} top={'50%'} left={'50%'} transform={'translate(-50%, -50%)'} zIndex={0} width={'inherit'} height={'inherit'}></Box>
-            <Box zIndex={1} position={'relative'} width={'inherit'} height={'inherit'}>
+        <Box
+            className="glassCard"
+            ref={ref}
+            position={'relative'}
+            borderRadius={'16px'}
+            overflow={'hidden'}
+            width={width}
+            height={height}
+            transition={'0.35s all'}
+            _hover={
+                isOnHoverActive
+                    ? {
+                          transform: 'rotate(0deg)',
+                      }
+                    : ''
+            }
+            {...props}
+        >
+            <Box
+                className="glassCardEffect"
+                layerStyle="glass"
+                position={'absolute'}
+                top={'50%'}
+                left={'50%'}
+                transform={'translate(-50%, -50%)'}
+                zIndex={0}
+                width={'inherit'}
+                height={'inherit'}
+            ></Box>
+            <Box className="glassCardWrapperChildren" zIndex={1} position={'relative'} width={'inherit'} height={'inherit'}>
                 {children}
             </Box>
         </Box>
--- components/infoCard.js
+++ components/infoCard.js
@@ -1,17 +1,25 @@
 import { Stack, Flex, Box, Heading, Text, Image } from '@chakra-ui/react';
 import GlassCard from './glassCard';
 
-export const InfoCard = ({ image, title, description }) => {
+export const InfoCard = ({ children, image, title, description, rotate = 0 }) => {
     return (
         <Flex gap={{ base: 6, md: 12 }} alignItems={'center'}>
-            <GlassCard width={{ base: '75px', sm: '100px' }} height={{ base: '75px', sm: '80px' }} position={'relative'} zIndex={2}>
-                <Box position={'relative'} overflow={'hidden'} borderRadius={4} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} padding={4}>
+            <GlassCard
+                isOnHoverActive={!!rotate}
+                transform={`translateX(0%) rotate(${rotate}deg)`}
+                width={{ base: '75px', sm: '100px' }}
+                height={{ base: '75px', sm: '80px' }}
+                position={'relative'}
+                zIndex={2}
+            >
+                <Box position={'relative'} overflow={'hidden'} borderRadius={4} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} padding={4} zIndex={3}>
                     <Box textAlign={'center'}>
                         <Box position={'relative'} height={'50px'} width={'full'} overflow={'hidden'}>
                             <Image alt={'Hero Image'} fit={'contain'} align={'center'} w={'100%'} h={'100%'} src={`./${image}.svg`} />
                         </Box>
                     </Box>
                 </Box>
+                {children}
             </GlassCard>
             <Stack flex={1} gap={1}>
                 <Heading lineHeight={1.1} fontWeight={600} fontSize={{ base: 'sm', sm: 'md', lg: 'lg' }} spacing={0}>
